fae839207ae8b7c8c1b14bd1cdd03ae4ffe6c8d0
Your Name <you@example.com>
2024-05-09 15:48:28 +0000

    added logo changer

--- jenkins/copylogo.sh
+++ jenkins/copylogo.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/bash
 
-echo $1
+case $1 in
+  main)
+    echo "hey maiin"
+    cp ./jenkins/main.svg ./src/logo.svg
+    ;;
+  dev)
+    echo "hey devv"
+    cp ./jenkins/dev.svg ./src/logo.svg
+    ;;
+esac
